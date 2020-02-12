5ac9b3b740fea8f85a91601afb14a1e0f9ad9bf9
shinesanthosh <53941514+shinesanthosh@users.noreply.github.com>
2020-02-12 23:50:38 +0530

    Added serial communication
    Communicates with arduino through serial port with a preloaded code

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,4 +1,7 @@
 import serial
+ser = serial.Serial('COM1')
+lightstate = [[1,0,0],[1,0,0],[1,0,0],[1,0,0]]
+pin = [[2,3,4],[5,6,7],[8,9,10],[11,12,13]]
 
 def control(veh):
     maxlim = 120
@@ -19,13 +22,22 @@ def control(veh):
         times.append(allotime)
     return times
 
+def setlight(listate,pino):
+    for i in range(0,len(pino)):
+        for j in range(0,3):
+            if listate[i][j] == 1:
+                ser.write('pinon '+str(pino[i][j])+'=')
+            else:
+                ser.write('pinoff '+str(pino[i][j])+'=')
+
 def light(state):
-    output = []
+    
     for i in range(0,len(state)):
         if state[i] == 2:
-            output.append('g')
+            lightstate[i] = [0,0,1]
         elif state[i] == 1:
-            output.append('y')
+            lightstate[i] = [0,1,0]
         else:
-            output.append('r')
-    print output
+            lightstate[i] = [1,0,0]
+        print lightstate
+        setlight(lightstate,pin)
