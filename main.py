3a45d99cb564e333f17d9a163df5b369eaedba59
shinesanthosh <53941514+shinesanthosh@users.noreply.github.com>
2020-02-13 00:42:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,19 @@ import time
 
 
 state = [0,0,0,0]
-retime = lightcontrol.control([3,4,0,6])
+retime = lightcontrol.control([1,1,1,1])
 ways = len(retime)
 
-for i in range(0,ways):
-    state[i] = 2
-    lightcontrol.light(state)
-    time.sleep(retime[i])
-    state[i] = 1
-    lightcontrol.light(state)
-    if retime[i] == 0:
-        time.sleep(0)
-    else:
-        time.sleep(5)
-    state[i] = 0
-    lightcontrol.light(state)
+while True:
+    for i in range(0,ways):
+        state[i] = 2
+        lightcontrol.light(state)
+        time.sleep(retime[i])
+        state[i] = 1
+        lightcontrol.light(state)
+        if retime[i] == 0:
+            time.sleep(0)
+        else:
+            time.sleep(5)
+        state[i] = 0
+        lightcontrol.light(state)
