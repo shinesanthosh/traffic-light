b646000cd124d0ddcc6e437d8db97ffb0f55b6e4
shinesanthosh <53941514+shinesanthosh@users.noreply.github.com>
2020-02-14 14:44:14 +0530

    Raspi pin control with RPi.GPIO

--- lightcontrol.py
+++ lightcontrol.py
@@ -1,4 +1,12 @@
-import serial
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) 
+
+lightstate = [[1,0,0],[1,0,0],[1,0,0],[1,0,0]]
+pin = [[2,3,4],[5,6,7],[8,9,10],[11,12,13]]
 
 def control(veh):
     maxlim = 120
@@ -19,13 +27,30 @@ def control(veh):
         times.append(allotime)
     return times
 
+def setlight(listate,pino):
+    for i in range(0,len(pino)):
+        for j in range(0,3):
+            if listate[i][j] == 1:
+                GPIO.output(pino[i][j], GPIO.HIGH)
+            else:
+                GPIO.output(pino[i][j], GPIO.LOW)
+                
+
+
+def setlight(listate,pino):
+    for i in range(0,len(pino)):
+        for j in range(0,3):
+            
+
+
 def light(state):
-    output = []
+    
     for i in range(0,len(state)):
         if state[i] == 2:
-            output.append('g')
+            lightstate[i] = [0,0,1]
         elif state[i] == 1:
-            output.append('y')
+            lightstate[i] = [0,1,0]
         else:
-            output.append('r')
-    print output
+            lightstate[i] = [1,0,0]
+        print lightstate
+        setlight(lightstate,pin)
